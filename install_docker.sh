b701b178af9891ea9fe40ac1a53bbea84d38408c
Jack Blacketter <jblacketter@gmail.com>
2018-02-02 15:13:52 -0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,4 @@
-url -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-cache policy docker-ce
